d6a0e3434eed0ebcab988cc3bbb91de7798dc61d
Pedro <pedromarraturra2005@gmail.com>
2024-06-27 06:57:22 -0300

    Small changes in the login/register function

--- src/groups/login_and_register.py
+++ src/groups/login_and_register.py
@@ -8,18 +8,24 @@ def login_and_register(db_path: str) -> dict:
         
         os.system("clear")
         
-        res = input("[1] LOGIN [2] REGISTER -> ")
+        res = input("[1] LOGIN [2] REGISTER [3] EXIT -> ")
         
         if res == "1": 
             os.system("clear")
             info = login(db_path)
             print("Login successful!")
-            sleep(3)
+            sleep(2)
             return info
             
         elif res == "2": 
             os.system("clear")
             register(db_path)
             print("User created!")
-            sleep(3)
+            sleep(2)
             continue
+        
+        elif res == "3":
+            os.system("clear")
+            print("Closing...")
+            sleep(2)
+            exit()
